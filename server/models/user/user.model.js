186d333131aae561c2088c00d9e79b0a0d0ee533
rumanstheddy <sumanthcob@gmail.com>
2024-01-22 12:13:04 -0500

    created user schema and model

---
+++ server/models/user/user.model.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+const userSchema = require("./user.schema");
+
+module.exports = mongoose.model("UserModel", userSchema);
---
+++ server/models/user/user.schema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+module.exports = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    userId: { type: String, required: true },
+    email: { type: String, required: true },
+    userType: { type: String, enum: ["User", "Admin"], default: "User" },
+    playScores: [
+      { type: mongoose.SchemaTypes.ObjectId, ref: "PlayScoreModel" },
+    ],
+    isSSreviewer: { type: Boolean, default: false },
+  },
+  { collection: "User" }
+);
