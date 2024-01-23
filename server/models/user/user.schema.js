20f907c610c64bf408ac6bd6ec1c377abb63f5bd
rumanstheddy <sumanthcob@gmail.com>
2024-01-23 17:39:34 -0500

    replaced email with userId field

--- server/models/user/user.schema.js
+++ server/models/user/user.schema.js
@@ -4,7 +4,6 @@ module.exports = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    userId: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     userType: { type: String, enum: ["User", "Admin"], default: "User" },
--- server/services/user/userService.js
+++ server/services/user/userService.js
@@ -1,7 +1,7 @@
 const userModel = require("../../models/user/user.model");
 
-const getByUserId = async (userId) =>
-  await userModel.findOne({ userId: userId });
+const getByUserEmail = async (email) =>
+  await userModel.findOne({ email: email });
 
 const createUser = async (user) => await userModel.create(user);
 
@@ -12,17 +12,17 @@ const getAllUsers = async () => await userModel.find();
 
 const getUserBySearch = async (searchQuery) => {
   const regexSearch = new RegExp(searchQuery, i);
-  return await userModel.find({ userId: regexSearch });
+  return await userModel.find({ email: regexSearch });
 };
 
-const updateUser = async (userId, userInfo) => {
-  return userModel.updateOne({ userId: userId }, { $set: { ...userInfo } });
+const updateUser = async (email, userInfo) => {
+  return userModel.updateOne({ email: email }, { $set: { ...userInfo } });
 };
 
 const deleteUserById = async (id) => await userModel.deleteOne({ _id: id });
 
 module.exports = {
-  getByUserId,
+  getByUserEmail,
   createUser,
   getAllUsers,
   getUserBySearch,
