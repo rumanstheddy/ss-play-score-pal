a9b82d928fe9198688f5ed28c290a66c302d70b8
rumanstheddy <sumanthcob@gmail.com>
2024-01-25 18:46:06 -0500

    schema tweaks for game and user

--- server/models/game/game.schema.js
+++ server/models/game/game.schema.js
@@ -5,7 +5,8 @@ module.exports = new mongoose.Schema(
     name: { type: String, required: true },
     developer: String,
     publisher: String,
-    Summary: { type: String, required: true },
+    releaseDate: new Date(),
+    summary: { type: String, required: true },
     genre: [String],
   },
   { collection: "Game" }
--- server/models/user/user.schema.js
+++ server/models/user/user.schema.js
@@ -4,7 +4,7 @@ module.exports = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     userType: { type: String, enum: ["User", "Admin"], default: "User" },
     isSSreviewer: { type: Boolean, default: false },
