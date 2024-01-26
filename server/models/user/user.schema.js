4c32ab1aa95399ff0b98df48deb8383537607a6e
rumanstheddy <sumanthcob@gmail.com>
2024-01-25 23:17:39 -0500

    set password character limit, email field as unique

--- server/models/user/user.schema.js
+++ server/models/user/user.schema.js
@@ -4,8 +4,8 @@ module.exports = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true, minlength: 6 },
     userType: { type: String, enum: ["User", "Admin"], default: "User" },
     isSSreviewer: { type: Boolean, default: false },
   },
