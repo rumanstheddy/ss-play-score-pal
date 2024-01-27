b2172609c8825b6fec1430460a2183cdf1b5ddfb
rumanstheddy <sumanthcob@gmail.com>
2024-01-27 11:34:02 -0500

    added schema validators

--- server/models/game/game.schema.js
+++ server/models/game/game.schema.js
@@ -2,11 +2,17 @@ const mongoose = require("mongoose");
 
 module.exports = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: {
+      type: String,
+      required: [true, "Please enter the name of the game"],
+    },
     developer: String,
     publisher: String,
     releaseDate: new Date(),
-    summary: { type: String, required: true },
+    summary: {
+      type: String,
+      required: [true, "Please enter a summary for the game"],
+    },
     genre: [String],
   },
   { collection: "Game" }
--- server/models/user/user.schema.js
+++ server/models/user/user.schema.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 module.exports = new mongoose.Schema(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true, minlength: 6 },
+    firstName: {
+      type: String,
+      required: [true, "Please enter your First Name"],
+    },
+    lastName: { type: String, required: [true, "Please enter your Last Name"] },
+    email: {
+      type: String,
+      required: [true, "Please enter an e-mail address"],
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "Please enter a valid e-mail address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter a password"],
+      minlength: [6, "Your password should be at least 6 characters in length"],
+    },
     userType: { type: String, enum: ["User", "Admin"], default: "User" },
     isSSreviewer: { type: Boolean, default: false },
   },
