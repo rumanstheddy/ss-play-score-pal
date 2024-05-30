433f46391a9a6c16ffa1a65360f19e7b11f39f52
rumanstheddy <sumanthcob@gmail.com>
2024-05-30 18:10:19 -0400

    signup now displays a success message

--- ui/src/app/login/page.tsx
+++ ui/src/app/login/page.tsx
@@ -8,7 +8,6 @@ export default function Login() {
   const [password, setPassword] = useState("");
 
   const onSubmit = async () => {
-
     const result = await signIn("credentials", {
       email: email,
       password: password,
@@ -20,37 +19,39 @@ export default function Login() {
   };
 
   return (
-    <>
-      <h1 className="text-4xl">Login</h1>
-      <label htmlFor="input_email" className="inline-block">
-        Email:
-      </label>
-      <input
-        name="input_email"
-        type="email"
-        className="block text-black"
-        placeholder="Enter your email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <label htmlFor="input_pw" className="inline-block">
-        Password:
-      </label>
-      <input
-        name="input_pw"
-        type="password"
-        className="block text-black"
-        placeholder="Enter your password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button
-        className="rounded-lg bg-white py-3 px-6 text-center align-middle text-black mt-3"
-        type="button"
-        onClick={() => onSubmit()}
-      >
-        Login
-      </button>
-    </>
+    <div className="flex justify-center min-h-screen">
+      <div className=" flex-col content-center">
+        <h1 className="text-4xl">Login</h1>
+        <label htmlFor="input_email" className="inline-block">
+          Email:
+        </label>
+        <input
+          name="input_email"
+          type="email"
+          className="block text-black"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="input_pw" className="inline-block">
+          Password:
+        </label>
+        <input
+          name="input_pw"
+          type="password"
+          className="block text-black"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="rounded-lg bg-white py-3 px-6 text-center align-middle text-black mt-3"
+          type="button"
+          onClick={() => onSubmit()}
+        >
+          Login
+        </button>
+      </div>
+    </div>
   );
 }
--- ui/src/app/signup/page.tsx
+++ ui/src/app/signup/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
+import { XCircleIcon } from "@heroicons/react/16/solid";
 
 export default function Signup() {
   const [fName, setFname] = useState("");
   const [lName, setLname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isSignupSuccess, setIsSignupSuccess] = useState(false);
+
+  const successMsgStr = "You've registered successfully!";
 
   const signUp = async () => {
     const body = {
@@ -25,68 +30,110 @@ export default function Signup() {
       body: JSON.stringify(body),
     });
 
-    return response.json();
+    return response;
+  };
+
+  const clearFields = () => {
+    setFname("");
+    setLname("");
+    setEmail("");
+    setPassword("");
   };
 
   const onSubmit = async () => {
-    const data = await signUp();
-    console.log(data);
+    const response = await signUp();
+    if (response.status === 201) setIsSignupSuccess(true);
+    console.log(response.status);
+    clearFields();
   };
 
+  const displaySuccessMsg = () =>
+    isSignupSuccess ? (
+      <div className="rounded-lg bg-green-500 px-4 py-1 mb-5">
+        {successMsgStr}
+        <Link
+          href={"/login"}
+          className="pl-2 text-blue-600 hover:underline hover:text-blue-800"
+        >
+          Login
+        </Link>
+        <XCircleIcon
+          className="size-4 inline-block ml-3 mb-0.5 hover:cursor-pointer"
+          onClick={() => setIsSignupSuccess(false)}
+        />
+      </div>
+    ) : (
+      <></>
+    );
+
   return (
-    <>
-      <h1 className="text-4xl">Sign Up</h1>
-      <label htmlFor="input_fName" className="inline-block">
-        First Name:
-      </label>
-      <input
-        name="input_fName"
-        type="text"
-        className="block text-black"
-        placeholder="Enter your First Name"
-        value={fName}
-        onChange={(e) => setFname(e.target.value)}
-      />
-      <label htmlFor="input_lName" className="inline-block">
-        Last Name:
-      </label>
-      <input
-        name="input_lName"
-        type="text"
-        className="block text-black"
-        placeholder="Enter your Last Name"
-        value={lName}
-        onChange={(e) => setLname(e.target.value)}
-      />
-      <label htmlFor="input_email" className="inline-block">
-        Email:
-      </label>
-      <input
-        name="input_email"
-        type="email"
-        className="block text-black"
-        placeholder="Enter your email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <label htmlFor="input_pw" className="inline-block">
-        Password:
-      </label>
-      <input
-        name="input_pw"
-        type="password"
-        className="block text-black"
-        placeholder="Enter your password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button
-        className="rounded-lg bg-white py-3 px-6 text-center align-middle text-black mt-3"
-        type="button"
-        onClick={() => onSubmit()}
-      >
-        Sign Up
-      </button>
-    </>
+    <div className="flex justify-center min-h-screen">
+      <div className="content-center">
+        {displaySuccessMsg()}
+        <h1 className="text-4xl text-center mb-4">Sign Up</h1>
+        <div className="flex flex-col">
+          <label htmlFor="input_fName" className="inline-block">
+            First Name:
+          </label>
+          <input
+            name="input_fName"
+            type="text"
+            className="text-black rounded-lg placeholder:pl-2"
+            placeholder="Enter your First Name"
+            value={fName}
+            onChange={(e) => setFname(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="input_lName" className="inline-block mt-2">
+            Last Name:
+          </label>
+          <input
+            name="input_lName"
+            type="text"
+            className="text-black rounded-lg placeholder:pl-2"
+            placeholder="Enter your Last Name"
+            value={lName}
+            onChange={(e) => setLname(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="input_email" className="inline-block mt-2">
+            Email:
+          </label>
+          <input
+            name="input_email"
+            type="email"
+            className="text-black rounded-lg placeholder:pl-2"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="input_pw" className="inline-block mt-2">
+            Password:
+          </label>
+          <input
+            name="input_pw"
+            type="password"
+            className="text-black rounded-lg placeholder:pl-2"
+            placeholder="Enter a password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex justify-center">
+          <button
+            className="rounded-lg bg-white py-3 px-6 text-center 
+            align-middle text-black mt-5"
+            type="button"
+            onClick={() => onSubmit()}
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
