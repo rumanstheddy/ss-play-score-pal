99205dd1263aa5c90ad6a86758750b9a5d0dea97
rumanstheddy <sumanthcob@gmail.com>
2024-06-03 12:29:33 -0400

    created bare bones navbar

--- ui/src/app/(home)/page.tsx
+++ ui/src/app/(home)/page.tsx
@@ -1,25 +1,31 @@
+import NavBar from "@/components/NavBar";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center min-h-screen">
-      <h1 className="text-4xl text-white mb-5 text-center">SS Playscore Pal</h1>
-      <span className="text-center">
-        <Link
-          className=" text-blue-500 hover:text-blue-700 text-center hover:underline"
-          href="/signup"
-        >
-          Sign Up
-        </Link>
-      </span>
-      <span className="text-center">
-        <Link
-          className=" text-blue-500 hover:text-blue-700 text-center hover:underline"
-          href="/login"
-        >
-          Login
-        </Link>
-      </span>
-    </div>
+    <>
+      <NavBar />
+      <div className="flex flex-col justify-center min-h-screen">
+        <h1 className="text-4xl text-white mb-5 text-center">
+          SS Playscore Pal
+        </h1>
+        <span className="text-center">
+          <Link
+            className=" text-blue-500 hover:text-blue-700 text-center hover:underline"
+            href="/signup"
+          >
+            Sign Up
+          </Link>
+        </span>
+        <span className="text-center">
+          <Link
+            className=" text-blue-500 hover:text-blue-700 text-center hover:underline"
+            href="/login"
+          >
+            Login
+          </Link>
+        </span>
+      </div>
+    </>
   );
 }
--- ui/src/components/NavBar.tsx
+++ ui/src/components/NavBar.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
-export default function NavBar() {
-  return <div>NavBar</div>;
+export default function NavBar(): React.ReactElement {
+  return (
+    <div className="fixed top-0 w-screen bg-gray-800 h-12">
+      <div className="flex flex-col justify-center h-12">
+        <div className="flex flex-row text-xl">
+          <span className="basis-1/4 text-center">Sumanth</span>
+          <span className="basis-2/4 text-center hover:underline">
+            <Link href="/">Home</Link>
+          </span>
+          <span className="basis-1/4 text-center text-red-700 hover:underline hover:text-red-800">
+            <Link href="">Logout</Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
