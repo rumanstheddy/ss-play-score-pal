b224c8d052ae10f62372e4fa09e57a99f4033a01
rumanstheddy <sumanthcob@gmail.com>
2024-06-05 16:03:03 -0400

    cleanup

--- ui/src/components/NavBar.tsx
+++ ui/src/components/NavBar.tsx
@@ -2,11 +2,7 @@ import { signOut } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
-interface InavBarProps {
-  name: string;
-}
-
-export default function NavBar({ name }: InavBarProps): React.ReactElement {
+export default function NavBar({ name }: { name: string }): React.ReactElement {
   return (
     <div className="fixed top-0 w-screen bg-gray-800 h-12">
       <div className="flex flex-col justify-center h-12">
