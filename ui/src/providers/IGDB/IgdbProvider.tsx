5770f7b7d249e8b5a29755a2ee610f983aecdeec
rumanstheddy <sumanthcob@gmail.com>
2024-07-01 10:54:48 -0400

    created provider functions for other endpoints

--- ui/src/providers/IGDB/IgdbProvider.tsx
+++ ui/src/providers/IGDB/IgdbProvider.tsx
@@ -19,29 +19,139 @@ type providerFnArgs = {
   filters?: string[];
 };
 
-const fetchGames = async ({
+const buildQuery = ({
   fields,
   limit,
   search,
   filters,
-}: providerFnArgs) => {
-  console.log("searchGame data: ");
-
+}: providerFnArgs): string => {
   let query: string = "fields " + (fields ? fields.join(",") : "*") + ";";
   query += limit ? "limit " + limit + ";" : "";
   query += search ? `search "${search}"` + ";" : "";
   query += filters ? "where " + filters.join(",") + ";" : "";
 
-  const response = await fetch(baseUrl + "games", {
+  return query;
+};
+
+const fetchData = async (url: string, query: string) => {
+  const response = await fetch(url, {
     method: "POST",
     headers: headers,
     body: query,
     mode: "cors",
   });
 
+  return response;
+};
+
+export const fetchGames = async ({
+  fields,
+  limit,
+  search,
+  filters,
+}: providerFnArgs) => {
+  const query = buildQuery({ fields, limit, search, filters });
+
+  const response = await fetchData(baseUrl + "games", query);
+
   const data = await response.json();
-  console.log("searchGame data: ", data);
   return data;
 };
 
-export { fetchGames };
+export const fetchScreenshots = async ({
+  fields,
+  limit,
+  search,
+  filters,
+}: providerFnArgs) => {
+  const query = buildQuery({ fields, limit, search, filters });
+
+  const response = await fetchData(baseUrl + "screenshots", query);
+
+  const data = await response.json();
+  return data;
+};
+
+export const fetchCovers = async ({
+  fields,
+  limit,
+  search,
+  filters,
+}: providerFnArgs) => {
+  const query = buildQuery({ fields, limit, search, filters });
+
+  const response = await fetchData(baseUrl + "covers", query);
+
+  const data = await response.json();
+  return data;
+};
+
+export const fetchReleaseDates = async ({
+  fields,
+  limit,
+  search,
+  filters,
+}: providerFnArgs) => {
+  const query = buildQuery({ fields, limit, search, filters });
+
+  const response = await fetchData(baseUrl + "release_dates", query);
+
+  const data = await response.json();
+  return data;
+};
+
+export const fetchGenresById = async ({
+  fields,
+  limit,
+  search,
+  filters,
+}: providerFnArgs) => {
+  const query = buildQuery({ fields, limit, search, filters });
+
+  const response = await fetchData(baseUrl + "genres", query);
+
+  const data = await response.json();
+  return data;
+};
+
+export const fetchInvolvedCompanies = async ({
+  fields,
+  limit,
+  search,
+  filters,
+}: providerFnArgs) => {
+  const query = buildQuery({ fields, limit, search, filters });
+
+  const response = await fetchData(baseUrl + "involved_companies", query);
+
+  const data = await response.json();
+  return data;
+};
+
+export const fetchCompanies = async ({
+  fields,
+  limit,
+  search,
+  filters,
+}: providerFnArgs) => {
+  const query = buildQuery({ fields, limit, search, filters });
+
+  const response = await fetchData(baseUrl + "companies", query);
+
+  const data = await response.json();
+  return data;
+};
+
+export const fetchPlatforms = async ({
+  fields,
+  limit,
+  search,
+  filters,
+}: providerFnArgs) => {
+  const query = buildQuery({ fields, limit, search, filters });
+
+  const response = await fetchData(baseUrl + "platforms", query);
+
+  const data = await response.json();
+  return data;
+};
