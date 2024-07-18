ffd9aa6b1e4276eb6d205b580297e6aeaa99b86d
rumanstheddy <sumanthcob@gmail.com>
2024-07-18 14:34:36 -0400

    cleaned up code

--- ui/src/views/GameInfo.tsx
+++ ui/src/views/GameInfo.tsx
@@ -7,7 +7,7 @@ import {
   fetchInvolvedCompanies,
 } from "@/providers/IGDB/IgdbProvider";
 import { useQuery } from "@tanstack/react-query";
-import { cp } from "fs";
+import Link from "next/link";
 
 interface IGameInfoProps {
   gameId: string;
@@ -77,26 +77,91 @@ export default function GameInfoView({
   type company = {
     id: number;
     company: number;
-    name: string;
-    developer: boolean;
-    publisher: boolean;
-    url: string;
+    name?: string;
+    developer?: boolean;
+    publisher?: boolean;
+    url?: string;
   };
 
-  const buildCompanyArr = () => {
-    const results = Array(companyNames?.length);
-    involvedCompanies?.forEach((company: company) => {
-      const compDetails: company = companyNames?.find(
-        (compInfo: company) => compInfo.id === company.company
-      );
+  const getCompanyDetails = (companyId: number) => {
+    return companyNames?.find((compInfo: company) => compInfo.id === companyId);
+  };
 
-      company.name = compDetails?.name;
-      company.url = compDetails?.url;
-
-      results.push(company);
+  const buildCompanyList = () => {
+    const companies = involvedCompanies?.map((company: company) => {
+      const compDetails: company = getCompanyDetails(company.company);
+      return {
+        id: compDetails?.id,
+        name: compDetails?.name,
+        url: compDetails?.url,
+        developer: company?.developer,
+        publisher: company?.publisher,
+      };
     });
 
-    return results;
+    return companies;
+  };
+
+  const separateDevAndPublishers = (companies: company[]) => {
+    const developers: Partial<company>[] = [];
+    const publishers: Partial<company>[] = [];
+
+    companies?.forEach((company: company) => {
+      if (company?.developer)
+        developers.push({
+          id: company.id,
+          url: company.url,
+          name: company.name,
+        });
+      if (company?.publisher)
+        publishers.push({
+          id: company.id,
+          url: company.url,
+          name: company.name,
+        });
+    });
+
+    return {
+      developers: developers,
+      publishers: publishers,
+    };
+  };
+
+  const displayCompanies = () => {
+    const { developers, publishers } = separateDevAndPublishers(
+      buildCompanyList()
+    );
+
+    console.log(separateDevAndPublishers(buildCompanyList()));
+
+    return (
+      <>
+        <ul className="list-disc list-inside marker:text">
+          Developed by:
+          {developers.map((company: Partial<company>) => {
+            return (
+              <li className="text-blue-500 underline" key={company.id}>
+                <Link className="hover:text-blue-700" href={`${company.url}`}>
+                  {company.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <ul className="list-disc list-inside marker:text">
+          Published by:
+          {publishers.map((company: Partial<company>) => {
+            return (
+              <li className="text-blue-500 underline" key={company.id}>
+                <Link className="hover:text-blue-700" href={`${company.url}`}>
+                  {company.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
   };
 
   // TODO: Find a way to display developers and publishers seperated by a ","
@@ -110,54 +175,7 @@ export default function GameInfoView({
         Genres:{" "}
         {genres ? genres.map((genre: genre) => genre.name).join(", ") : ""}
       </div>
-      <div className="text">
-        Companies:{" "}
-        {/* {involvedCompanies
-          ? involvedCompanies.map((company: company) => (
-              <div className="block" key={company.company}>
-                <span>ID: {company.company}</span>;
-                {company.developer ? (
-                  <span>Developer: {company.developer.toString()}</span>
-                ) : (
-                  <span>Publisher: {company.publisher.toString()}</span>
-                )}
-              </div>
-            ))
-          : ""}
-        <div className="text">
-          Company Names:{" "}
-          {companyNames
-            ? companyNames.map((company: company) => company.name).join(", ")
-            : ""}
-        </div> */}
-        {buildCompanyArr()
-          ? buildCompanyArr().map((company: company) => {
-              // const developers = [];
-              // const publishers = [];
-              // if (company?.developer) developers.push(company.name);
-              // if (company?.publisher) publishers.push(company.name);
-              // return (
-              //   <>
-              //     <div className="text block">
-              //       Developed by: {developers.join(", ")}
-              //     </div>
-              //     <div className="text block">
-              //       Published by: {publishers.join(", ")}
-              //     </div>
-              //   </>
-              // );
-              // <div className="block" key={company.company}>
-              //   Developer(s):{" "}
-              //   {company.developer ? (
-              //     <span>Developer: {company.name.toString()}</span>
-              //   ) : (
-              //     <span>Publisher: {company.name.toString()}</span>
-              //   )}
-              // </div>;
-              console.log(company);
-            })
-          : ""}
-      </div>
+      <div className="text">{displayCompanies()}</div>
     </>
   );
 }
